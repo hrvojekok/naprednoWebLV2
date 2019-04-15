8e1baf5193a37267773c229169d899ddee546c8c
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2019-04-15 19:21:30 +0200

    update

--- getImages.php
+++
@@ -1,8 +0,0 @@
-<?php
-$newPath=$_SERVER['DOCUMENT_ROOT'] ."/kokosarevicLV2/images/*.sha256";
-foreach($newPath as $filename){
-     echo $filename;
- }
-
-
-?>
--- lv2zad2.php
+++ lv2zad2.php
@@ -1,12 +1,42 @@
-<!DOCTYPE html>
-<html>
+<?php
+$files = @$_FILES["files"];
+
+if($files["name"] != '') {
+
+  $newPath=$_SERVER['DOCUMENT_ROOT'] ."/kokosarevicLV2/images/";
+  $fullPath = $_REQUEST["path"].$files["name"];
+  $realFullPath = $newPath . $fullPath;
+  echo($fullPath . "<br><br>");
+
+    if(move_uploaded_file($files['tmp_name'], $realFullPath)) {
+
+      //echo($fileName);
+      $put = realpath($fullPath);
+
+      $imageString = file_get_contents($put);
+      $encryptedImage = crypt($imageString, '$5$somethin$');
+
+      echo($encryptedImage ."<br><br>");
+      echo($realFullPath);
+      $encryptedName = substr($realFullPath, 0, -4);
+
+      $myfile = fopen("$encryptedName.sha256", "w") or die("Unable to open file!");
+      fwrite($myfile, $encryptedImage);
+      fclose($myfile);
+
+
+      echo "<h1><a href='$fullPath'>OK-Click here!</a></h1>";
+    }
+}
+echo '<html>
+<head>
+<title>Upload files...</title>
+</head>
 <body>
-
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload" accept="image/jpeg, image/png, application/pdf">
-    <input type="submit" value="Upload Image" name="submit">
+<form method=POST enctype="multipart/form-data" action="">
+<input type=text name=path>
+<input type="file" name="files">
+<input type=submit value="Up">
 </form>
-
-</body>
-</html>
+</body></html>';
+?>
