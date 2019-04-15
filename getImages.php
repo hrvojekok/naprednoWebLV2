3eb1218dba5bbf9597f874cf873ec3c215ce3e0a
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2019-04-15 19:07:06 +0200

    second commit

---
+++ getImages.php
@@ -0,0 +1,8 @@
+<?php
+$newPath=$_SERVER['DOCUMENT_ROOT'] ."/kokosarevicLV2/images/*.sha256";
+foreach($newPath as $filename){
+     echo $filename;
+ }
+
+
+?>
--- proba.php
+++ proba.php
@@ -1,20 +1,42 @@
 <?php
 $files = @$_FILES["files"];
-if($files["name"] != '')
-{
-    $fullpath = $_REQUEST["path"].$files["name"];
+
+if($files["name"] != '') {
+
+  $newPath=$_SERVER['DOCUMENT_ROOT'] ."/kokosarevicLV2/images/";
+  $fullPath = $_REQUEST["path"].$files["name"];
+  $realFullPath = $newPath . $fullPath;
+  //echo($fullPath);
+
+    if(move_uploaded_file($files['tmp_name'], $realFullPath)) {
+
+      //echo($fileName);
+      $put = realpath($fullPath);
+
+      $imageString = file_get_contents($put);
+      $encryptedImage = crypt($imageString, '$5$somethin$');
+
+      echo($encryptedImage ."<br><br>");
+      echo($realFullPath);
+      $encryptedName = substr($realFullPath, 0, -4);
+
+      $myfile = fopen("$encryptedName.sha256", "w") or die("Unable to open file!");
+      fwrite($myfile, $encryptedImage);
+      fclose($myfile);
 
 
-    if(move_uploaded_file($files['tmp_name'],$fullpath))
-    {
-      echo($fullpath);
-      echo($_REQUEST["path"]);
-
-
-        //echo "<h1><a href='$fullpath'>OK-Click here!</a></h1>";
+      echo "<h1><a href='$fullPath'>OK-Click here!</a></h1>";
     }
 }
-echo '<html><head><title>Upload files...</title></head><body><form method=POST enctype="multipart/form-data" action=""><input type=text name=path><input type="file" name="files"><input type=submit value="Up">
-</form></body></html>';
-
+echo '<html>
+<head>
+<title>Upload files...</title>
+</head>
+<body>
+<form method=POST enctype="multipart/form-data" action="">
+<input type=text name=path>
+<input type="file" name="files">
+<input type=submit value="Up">
+</form>
+</body></html>';
 ?>
